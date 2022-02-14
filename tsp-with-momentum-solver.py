61e877e6cf21741e279a805b4740d93da91f5694
Mike Garmulewicz <garmulewicz@gmail.com>
2022-02-13 22:38:25 -0800

    Added solution extractor

--- tsp-with-momentum-solver.py
+++ tsp-with-momentum-solver.py
@@ -1,13 +1,19 @@
 import attr
-from typing import List, Tuple
+import copy
+from typing import List, Tuple, Dict
 import numpy as np
+from brainos.utils.result import Result
 from ortools.linear_solver import pywraplp
 
+
+
+Edge = Tuple[int, int, int]
+Routing = Dict[Edge, List[Edge]]
 
 @attr.s(auto_attribs=True)
 class FunkyGraph:
     nodes_n: int
-    edges: List[Tuple[int, int, int]]
+    edges: List[Edge]
     costs: List[float]
 
 
@@ -116,8 +122,8 @@ def to_visits_constaint_matrix(edges, nodes_n):
     B = np.zeros(shape=(nodes_n, len(edges)), dtype=np.int32)
     for eix, edge in enumerate(edges):
         i, j, k = edge
-        B[i, eix] = 1
         B[j, eix] = 1
+        B[i, eix] = 1
         B[k, eix] = 1
 
     return B
@@ -164,9 +170,13 @@ def solve_traversal(graph: FunkyGraph):
     if status == pywraplp.Solver.OPTIMAL:
         print('Solution:')
         print('Objective value =', solver.Objective().Value())
-        for x in edge_variables:
+        solution = []
+        for x, edge in zip(edge_variables, graph.edges):
+            for _ in range(int(x.solution_value())):
+                solution.append(edge)
             print(f'Edge {x.name()} traversed {x.solution_value()} times')
     else:
+        solution = Result.error("Didn't find solution")
         print('The problem does not have an optimal solution.')
 
     print("     ")
@@ -174,6 +184,89 @@ def solve_traversal(graph: FunkyGraph):
     print('Problem solved in %d iterations' % solver.iterations())
     print('Problem solved in %d branch-and-bound nodes' % solver.nodes())
 
+    return solution
+
+
+# solution extraction
+
+def add_to_data(from_edge: Edge, to_edge: Edge, data: Routing):
+    if from_edge in data:
+        data[from_edge].append(to_edge)
+    else:
+        data[from_edge] = [to_edge]
+
+
+def remove_from_data(from_edge: Edge, to_edge: Edge, data: Routing):
+    assert from_edge in data
+    data[from_edge].remove(to_edge)
+
+
+def is_empty(data: Routing):
+    return all([len(to_edges) == 0 for to_edges in data.values()])
+
+
+def almost_done(data: Routing):
+    return sum([len(to_edges) for to_edges in data.values()]) == 1
+
+
+def resolve_solution(start_from: int, from_to: Routing) -> List[int]:
+    beginning_choices: List[Edge] = [(i, j) for i, j in from_to.keys() if i == start_from]
+
+    assert len(beginning_choices) >= 1
+
+    def traverse_edge(current_edge: Edge, from_to: Routing, solution: List[Edge]) -> Tuple[bool, List[Edge]]:
+        if len(from_to[current_edge]) == 0:
+            return False, solution
+        elif almost_done(from_to):
+            next_edge = from_to[current_edge][0]
+            solution.append(next_edge)
+            return True, solution
+        else:
+            possible_next_edges = copy.deepcopy(from_to[current_edge])
+            for possible_next_edge in possible_next_edges:
+                remove_from_data(current_edge, possible_next_edge, from_to)
+                solution.append(possible_next_edge)
+
+                success, solution = traverse_edge(possible_next_edge, from_to, solution)
+                if success:
+                    return True, solution
+
+                # add edge back to the data
+                add_to_data(current_edge, possible_next_edge, from_to)
+                # remove the edge from current solution candidate
+                solution.pop()
+
+            return False, solution
+
+    solution_so_far = []
+    for start_edge in beginning_choices:
+        solution_so_far.append(start_edge)
+        success, solution = traverse_edge(start_edge, from_to, solution_so_far)
+        if success:
+            break
+        else:
+            solution_so_far.pop()
+
+    if success:
+        # unpack solution
+        solution_nodes = []
+        for i, j in solution:
+            solution_nodes.append(i)
+        return solution_nodes
+    else:
+        raise AssertionError("Failed to find solution")
+    pass
+
 
 if __name__ == '__main__':
-    solve_traversal(PETERS)
+    graph = PETERS
+    solution = solve_traversal(graph)
+    print(solution)
+
+    from_to = {}
+    for i, j, k in solution:
+        add_to_data(from_edge=(i, j), to_edge=(j, k), data=from_to)
+
+    for start_from in range(graph.nodes_n):
+        solution = resolve_solution(start_from, copy.deepcopy(from_to))
+        print(f'Solution starting from {start_from} is {solution}')
